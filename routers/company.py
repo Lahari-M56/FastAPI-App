c9d84931ca5075056b42d6355b9626965a531757
Lahari-M56 <laharimurugesh2005@gmail.com>
2026-06-29 10:04:29 +0530

    FastAPI_schemas

--- routers/company.py
+++ routers/company.py
@@ -10,7 +10,6 @@ router = APIRouter(
     tags=["company"]
 )
 
-# Create Company
 @router.post("/", response_model=CompanyResponse, status_code=status.HTTP_201_CREATED)
 def create_company(company: CompanyCreate, db: Session = Depends(get_db)):
     db_company = Company(**company.model_dump())
@@ -20,51 +19,41 @@ def create_company(company: CompanyCreate, db: Session = Depends(get_db)):
     return db_company
 
 
-# Get All Companies
 @router.get("/", response_model=list[CompanyResponse])
 def get_all_company(db: Session = Depends(get_db)):
-    return db.query(Company).all()
+    companies=db.query(Company).all()
+    return companies
 
 
-# Get Company by ID
 @router.get("/{company_id}", response_model=CompanyResponse)
 def get_company(company_id: int, db: Session = Depends(get_db)):
     company = db.query(Company).filter(Company.id == company_id).first()
 
-    if company is None:
+    if not company:
         raise HTTPException(status_code=404, detail="Company not found")
 
     return company
 
 
-# Update Company
 @router.put("/{company_id}", response_model=CompanyResponse)
 def update_company(company_id: int, company: CompanyUpdate, db: Session = Depends(get_db)):
     db_company = db.query(Company).filter(Company.id == company_id).first()
-
-    if db_company is None:
+    if not db_company :
         raise HTTPException(status_code=404, detail="Company not found")
-
     update_data = company.model_dump(exclude_unset=True)
-
     for key, value in update_data.items():
         setattr(db_company, key, value)
-
     db.commit()
     db.refresh(db_company)
-
     return db_company
 
 
-# Delete Company
 @router.delete("/{company_id}")
 def delete_company(company_id: int, db: Session = Depends(get_db)):
     db_company = db.query(Company).filter(Company.id == company_id).first()
 
-    if db_company is None:
-        raise HTTPException(status_code=404, detail="Company not found")
-
+    if not db_company :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
     db.delete(db_company)
     db.commit()
-
     return {"message": "Company deleted successfully"}
